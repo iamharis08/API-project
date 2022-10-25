b19788613cb01047884940ee3e7db157a5802d30
Haris Ahmed <iamharis08@gmail.com>
2022-10-25 13:01:07 -0400

    user first and last name added

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -64,6 +64,14 @@ module.exports = (sequelize, DataTypes) => {
           }
         }
       },
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
       email: {
         type: DataTypes.STRING,
         allowNull: false,
