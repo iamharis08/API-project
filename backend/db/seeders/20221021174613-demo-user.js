762b2c347db2e6fe8ab11478927f70e1bfb83d7a
Haris Ahmed <iamharis08@gmail.com>
2022-10-25 13:08:06 -0400

    adding name seeds

--- backend/db/seeders/20221021174613-demo-user.js
+++ backend/db/seeders/20221021174613-demo-user.js
@@ -8,16 +8,22 @@ module.exports = {
       {
         email: 'demo@user.io',
         username: 'Demo-lition',
+        firstName: 'Hanzenberg',
+        lastName: 'White',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
         email: 'user1@user.io',
         username: 'FakeUser1',
+        firstName: 'Bugs',
+        lastName: 'Bunny',
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
         email: 'user2@user.io',
         username: 'FakeUser2',
+        firstName: 'Harry',
+        lastName: 'Houdini',
         hashedPassword: bcrypt.hashSync('password3')
       }
     ], {});
