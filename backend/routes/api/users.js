da275786a5fc17599a0f39d7b8463c8526127e7a
Haris Ahmed <iamharis08@gmail.com>
2022-10-26 00:20:57 -0400

    user-names repair and model refactoring

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -37,8 +37,8 @@ router.post(
     '/',
     validateSignup,
     async (req, res) => {
-      const { email, password, username, firstName, lastName } = req.body;
-      const user = await User.signup({ email, username, password, firstName, lastName});
+      const { firstName, lastName, email, password, username } = req.body;
+      const user = await User.signup({firstName, lastName, email, username, password, });
 
       await setTokenCookie(res, user);
 
