9e1b44e5808a3315d95c2b496975921cd7193a2b
Haris Ahmed <iamharis08@gmail.com>
2022-10-27 14:07:33 -0400

    get all users and get all current users spots using javascript final

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -15,72 +15,143 @@ router.get('/', async (req, res) => {
     const spots = await Spot.findAll({
         include: [{
             model: Review,
-            attributes: [] // don't return any associated data from Challenges
         },
         {
             model: SpotImage,
-            attributes: []
         }
     ],
-        attributes: {
-            include: [
-                [
-                    //Adding AVG of Stars Rating of each spot
-                    sequelize.fn("AVG", sequelize.col("Reviews.stars")),
-                    "avgRating"
-                ],
-                    //Adding column of Spot Images Url aliased as previewImage
-                [
-                    sequelize.col("SpotImages.url"),
-                    "previewImage"
-                ]
-            ],
-        },
+        // attributes: {
+        //     include: [
+        //         [
+        //             //Adding AVG of Stars Rating of each spot
+        //             sequelize.fn("AVG", sequelize.col("Reviews.stars")),
+        //             "avgRating"
+        //         ],
+        //             //Adding column of Spot Images Url aliased as previewImage
+        //         [
+        //             sequelize.col("SpotImages.url"),
+        //             "previewImage"
+        //         ]
+        //     ],
+        // },
 
-
-        group: ["address", "city", "state", "country"],
+        order: ['id'],
+        // group: ["address", "city", "state", "country"],
     })
+    const spotStars = {};
+
+
+
+    for (let i = 0; i < spots.length; i++) {
+        let spot = spots[i];
+        let reviewsArray = spot.Reviews;
+        let sum = 0;
+
+        if (reviewsArray.length !== 0) {
+
+            for (let j = 0; j < reviewsArray.length; j++){
+
+                let reviewStars = reviewsArray[j].stars;
+
+                    sum += reviewStars;
+
+            }
+        } else spotStars[spot.id] = 0
+
+        if (sum !== 0) spotStars[spot.id] = sum / reviewsArray.length
+
+        delete spots[i].dataValues.Reviews;
+        spots[i].dataValues.avgRating = spotStars[spot.id]
+
+
+
+
+if (spots[i].dataValues.SpotImages.length) {
+    let previewImage = spots[i].dataValues.SpotImages[0].url
+    spots[i].dataValues.previewImage = previewImage
+}else spots[i].dataValues.previewImage = ''
+delete spots[i].dataValues.SpotImages
+
+    }
+
 
     res.json({
-        Spots: spots,
+         Spots: spots,
     })
 });
 
+
+
 router.get('/current', requireAuth, async (req, res) => {
-    const currentUserSpots = await Spot.findAll({
+    const spots = await Spot.findAll({
         where: {
-            ownerId: req.user.dataValues.id
+            ownerId: req.user.id
         },
         include: [{
             model: Review,
-            attributes: [] 
         },
         {
             model: SpotImage,
-            attributes: []
         }
     ],
-        attributes: {
-            include: [
-                [
-                    //Adding AVG of Stars Rating of each spot
-                    sequelize.fn("AVG", sequelize.col("Reviews.stars")),
-                    "avgRating"
-                ],
-                    //Adding column of Spot Images Url aliased as previewImage
-                [
-                    sequelize.col("SpotImages.url"),
-                    "previewImage"
-                ]
-            ],
-        },
+        // attributes: {
+        //     include: [
+        //         [
+        //             //Adding AVG of Stars Rating of each spot
+        //             sequelize.fn("AVG", sequelize.col("Reviews.stars")),
+        //             "avgRating"
+        //         ],
+        //             //Adding column of Spot Images Url aliased as previewImage
+        //         [
+        //             sequelize.col("SpotImages.url"),
+        //             "previewImage"
+        //         ]
+        //     ],
+        // },
 
 
-        group: ["address", "city", "state", "country"],
+        // group: ["address", "city", "state", "country"],
     })
+    const spotStars = {};
+
+
+
+    for (let i = 0; i < spots.length; i++) {
+        let spot = spots[i];
+        let reviewsArray = spot.Reviews;
+        let sum = 0;
+
+        if (reviewsArray.length !== 0) {
+
+            for (let j = 0; j < reviewsArray.length; j++){
+
+                let reviewStars = reviewsArray[j].stars;
+
+                    sum += reviewStars;
+
+            }
+        } else spotStars[spot.id] = 0
+
+        if (sum !== 0) spotStars[spot.id] = sum / reviewsArray.length
+
+        delete spots[i].dataValues.Reviews;
+        spots[i].dataValues.avgRating = spotStars[spot.id]
+
+
+
+
+        if (spots[i].dataValues.SpotImages.length) {
+            let previewImage = spots[i].dataValues.SpotImages[0].url
+            spots[i].dataValues.previewImage = previewImage
+        }else spots[i].dataValues.previewImage = ''
+
+        
+        delete spots[i].dataValues.SpotImages
+
+    }
 
     res.json({
-        Spots: currentUserSpots
+        Spots: spots
     })
 
 
