42c335a65efd909fb5156247f19552f78887c1bd
Haris Ahmed <iamharis08@gmail.com>
2022-11-18 17:25:15 -0500

    debugging grid layout spots

--- frontend/src/components/Spots/Spots.js
+++ frontend/src/components/Spots/Spots.js
@@ -3,7 +3,7 @@ import { Redirect, useHistory, NavLink } from "react-router-dom";
 import "./Spots.css";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchAllSpots } from "../../store/spots";
-
+import stars from "../SpotDetailsPage/SpotDetailsPageImages/stars.svg"
 function Spots() {
   const dispatch = useDispatch();
   const history = useHistory();
@@ -17,35 +17,41 @@ function Spots() {
     dispatch(fetchAllSpots());
   }, [dispatch]);
 
-  if (!spots)
-    return (
-      null
-    );
+  if (!spots) return null;
   return (
     <div className="spots">
-      {Object.values(spots).map(spot =>
-          <div key={spot.id}>
-            <NavLink
-              style={{ textDecoration: "none", color: "black" }}
-              to={`/spots/${spot.id}`}
-            >
-              <div className="spots-container" onClick={handleClick(spot.id)}>
-                <div className="spot-img">
-                  <img src={spot.previewImage ? spot.previewImage : "https://st4.depositphotos.com/14953852/24787/v/600/depositphotos_247872612-stock-illustration-no-image-available-icon-vector.jpg"} alt="spots" />
+      {Object.values(spots).map((spot) => (
+        <div key={spot.id}>
+          <NavLink
+            style={{ textDecoration: "none", color: "black" }}
+            to={`/spots/${spot.id}`}
+          >
+            <div className="spots-container" onClick={handleClick(spot.id)}>
+              <div className="spot-img">
+                <img
+                  src={
+                    spot.previewImage
+                      ? spot.previewImage
+                      : "https://st4.depositphotos.com/14953852/24787/v/600/depositphotos_247872612-stock-illustration-no-image-available-icon-vector.jpg"
+                  }
+                  alt="spots"
+                />
+              </div>
+
+              <div className="spots-info">
+                <div className="spots-address">
+                  {`${spot.city}, ${spot.state}`}
+                  <span className="spots-stars"><span className="star"><img src={stars} alt="stars" /></span>{spot.avgRating}</span>
                 </div>
 
-                <div className="spots-info">
-                  <div id="spots-address">{`${spot.city}, ${spot.state}`}</div>
-                  <div id="spots-stars">{`Stars: ${spot.avgRating}`}</div>
-                  <div id="spots-price">
-                    <span>{`$${spot.price} `}</span>night
-                  </div>
+                <div className="spots-price">
+                  <span>{`$${spot.price} `}</span>night
                 </div>
               </div>
-            </NavLink>
-          </div>
-
-      )}
+            </div>
+          </NavLink>
+        </div>
+      ))}
     </div>
   );
 }
